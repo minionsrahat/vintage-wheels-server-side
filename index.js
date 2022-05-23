548e88c3c1a552bf8b2d828809a7562bbeee2397
minionsrahat <rahatuddin786@gmail.com>
2022-05-23 18:39:23 +0600

    login api created

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@carsrus.9w4iz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.PASSWORD}@vintagewheels.y6twe.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const verifyRequest = (req, res, next) => {
     const tokenInfo = req.headers.accesstoken;
@@ -43,14 +44,18 @@ const verifyRequest = (req, res, next) => {
 async function run() {
     try {
         await client.connect();
-        const database = client.db("CarsRUs");
-        const carsdata = database.collection("carsdata");
+        const database = client.db("vintage-wheels");
+        const toolsdata = database.collection("toolsData");
+        const users = database.collection("users");
         console.log('Db connected')
 
 
         // auth
         app.post('/login', async (req, res) => {
-            const email = req.body.email
+            const {email, name} = req.body
+            if(name){
+                const result = await users.insertOne(req.body)
+            }
             const token = jwt.sign(email, process.env.ACCESS_TOKEN);
             res.send({ token })
         })
@@ -59,7 +64,7 @@ async function run() {
         app.post('/addCarsData', async (req, res) => {
             const newcar = req.body;
             // console.log(newcar);
-            const result = await carsdata.insertOne(newcar)
+            const result = await toolsdata.insertOne(newcar)
             // console.log("add user :" + user);
             res.send(result)
         })
@@ -70,13 +75,13 @@ async function run() {
             console.log(req.query);
             let result;
             if (limit) {
-                result = await carsdata.find({}).limit(parseInt(limit))
+                result = await toolsdata.find({}).limit(parseInt(limit))
             }
             else if (email) {
-                result = await carsdata.find({email:email})
+                result = await toolsdata.find({email:email})
             }
             else {
-                result = await carsdata.find({})
+                result = await toolsdata.find({})
 
             }
             res.send(await result.toArray())
@@ -86,7 +91,7 @@ async function run() {
             const email = req.query.email
             let result;
             if (email) {
-                result = await carsdata.find({email:email})
+                result = await toolsdata.find({email:email})
                 
             }
             res.send(await result.toArray())
@@ -98,26 +103,26 @@ async function run() {
 
             const id = req.params.id
             const query = { _id: ObjectId(id) }
-            const result = await carsdata.findOne(query)
+            const result = await toolsdata.findOne(query)
             res.send(result)
         })
 
         app.put('/deliverCarData/:id',verifyRequest, async (req, res) => {
             const id = req.params.id
             const filter = { _id: ObjectId(id) }
-            const singleCar = await carsdata.findOne(filter)
+            const singleCar = await toolsdata.findOne(filter)
             const options = { upsert: true };
             const updateDoc = {
                 $set: {quantity:parseInt(singleCar.quantity)-1},
             };
-            const result = await carsdata.updateOne(filter, updateDoc, options);
+            const result = await toolsdata.updateOne(filter, updateDoc, options);
             res.send(result)
         })
 
         app.delete('/deleteCarData/:id',verifyRequest, async (req, res) => {
             const id=req.params.id
             const query={_id:ObjectId(id)}
-            const result=await carsdata.deleteOne(query)
+            const result=await toolsdata.deleteOne(query)
             res.send(result)
          })
 
@@ -126,12 +131,12 @@ async function run() {
             const newQuantity = req.body.stock
             console.log(req.body);
             const filter = { _id: ObjectId(id) }
-            const singleCar = await carsdata.findOne(filter)
+            const singleCar = await toolsdata.findOne(filter)
             const options = { upsert: true };
             const updateDoc = {
                 $set: {quantity:parseInt(singleCar?.quantity)+newQuantity},
             };
-            const result = await carsdata.updateOne(filter, updateDoc, options);
+            const result = await toolsdata.updateOne(filter, updateDoc, options);
             res.send(result)
         })
     } finally {
